a515abbb4e1d73a8a842be7da9799979713d1ea0
trask <trask@ndprotascik.trask.cz>
2023-11-19 19:52:41 +0100

    init commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "snbx12" {
+
+  name     = "Sandbox12"
+  location = "North Europe"
+
+}
+
+resource "azurerm_service_plan" "sndbx12_plan" {
+  name                = "sandbox12-sp"
+  resource_group_name = azurerm_resource_group.snbx12.name
+  location            = azurerm_resource_group.snbx12.location
+  os_type             = "Linux" 
+  sku_name            = "F1"   
+}
+
+resource "azurerm_linux_web_app" "sndbx12_app" {
+  name                = "dmipro"
+  resource_group_name = azurerm_resource_group.snbx12.name
+  location            = azurerm_service_plan.sndbx12_plan.location
+  service_plan_id     = azurerm_service_plan.sndbx12_plan.id
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = ""
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = ""
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    always_on = false
+    application_stack {
+
+      docker_image     = "httpd"
+      docker_image_tag = "latest"
+
+
+    }
+
+  }
+
+
+
+}
